e655306be836ebae37df85cbcabf3c3abdbb7042
an9383 <an9383@gmail.com>
2025-04-26 13:46:23 +0900

    all

--- lesson6/0426_03_JUST_SHOPMALL_ROUTER/src/router/index.js
+++ lesson6/0426_03_JUST_SHOPMALL_ROUTER/src/router/index.js
@@ -1,7 +1,16 @@
 import { createRouter } from 'vue-router';
 
+const routes = [
+    // 메인
+    // 서브1
+    // 서브2
+    // 서브3
+    // 서브4
+]
+
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
 
