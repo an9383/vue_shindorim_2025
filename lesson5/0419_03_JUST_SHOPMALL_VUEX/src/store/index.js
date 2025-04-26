9d42499fb655abeb0803391d6d216d69742f58ba
an9383 <an9383@gmail.com>
2025-04-26 09:56:23 +0900

    all

--- lesson5/0419_03_JUST_SHOPMALL_VUEX/src/store/index.js
+++ lesson5/0419_03_JUST_SHOPMALL_VUEX/src/store/index.js
@@ -1,10 +1,24 @@
 import { createStore } from "vuex";
 
-const store = createStore({
-    state(){
+const store = createStore({             //객체(Object)형태로 저장
+    state() {                            //state는 데이터를 저장하는 공간으로 메소드형태로 저장 state: function() {}
         return {
             isShow: false,
-            cnt: 0
+            cnt: 0,
+            main:{
+                section1: {
+                cnt: 0,
+                isClass1: false
+            },
+            section2: {
+                공지사항: [],
+                갤러리: []
+            },
+            },            
+            header: {
+                gnb: [],
+                logo: '',
+            }
         }
     },
     getters: {},
